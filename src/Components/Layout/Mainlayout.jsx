bd2e59d40109367a813e76a3138f60d8d8e79a70
Sabbirahmed0007 <>
2023-11-09 04:12:29 +0600

    added footer

--- src/Components/Layout/Mainlayout.jsx
+++ src/Components/Layout/Mainlayout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../Header/Navbar';
 import { Outlet } from 'react-router-dom';
+import Footer from '../Footer/Footer';
 
 const Mainlayout = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
