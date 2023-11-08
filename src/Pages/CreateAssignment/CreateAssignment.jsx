b61c8af1d59bfa24a2c30e6317811b2624a14a6d
Sabbirahmed0007 <>
2023-11-09 01:13:52 +0600

    sent data of creating assignment to backend side

--- src/Pages/CreateAssignment/CreateAssignment.jsx
+++ src/Pages/CreateAssignment/CreateAssignment.jsx
@@ -1,11 +1,40 @@
+import axios from 'axios';
 import React, { useState } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import Swal from 'sweetalert2';
 
 const CreateAssignment = () => {
 
 
     const [selectedDate, setSelectedDate]= useState(null);
+
+    const handleCreateAsignment=(e)=>{
+        e.preventDefault();
+        const form= e.target;
+
+        const title= form.title.value;
+        const description= form.description.value;
+        const img= form.img.value;
+        const level= form.level.value;
+        const marks= form.marks.value;
+        const date= form.date.value;
+        const infos= {title, description, img, level, marks, date};
+        console.log(infos);
+
+        axios.post('http://localhost:5000/createassignments', infos, {withCredentials:true})
+        .then(res=>{
+            console.log(res.data);
+            if(res.data.insertedId){
+                Swal.fire('','Assignment has been created successfully', 'success');
+            }
+        })
+        .catch(error=>{
+            console.log(error);
+        })
+
+
+    }
 
 
 
@@ -15,7 +44,7 @@ const CreateAssignment = () => {
             <div className='text-center'>
                 <h2 className='font-bold text-3xl my-7'>Create Your Assignment</h2>
             </div>
-                <form className='w-full px-4  '>
+                <form onSubmit={handleCreateAsignment} className='w-full px-4 '>
                     <div className='my-2'>
                         <label htmlFor="title" className='font-medium text-lg'>Title Of The Assignment</label><br />
                         <input type="text" name="title" id="title" className='w-full  p-2 rounded-md  bg-gray-200'  placeholder="Leave the assignment's title"/>
@@ -43,11 +72,11 @@ const CreateAssignment = () => {
                         </div>
                         <div className='w-full'>
                             <label htmlFor="marks" className='font-medium text-lg'>Date</label><br />
-                            <DatePicker name="date" selected={selectedDate} onChange={(date)=>setSelectedDate(date)} minDate={new Date()} className='w-full p-2 rounded-md  bg-gray-200'></DatePicker>
+                            <DatePicker name="date" dateFormat={'dd/MM/yyyy'} selected={selectedDate} onChange={(date)=>setSelectedDate(date)} minDate={new Date()} className='w-full p-2 rounded-md  bg-gray-200'></DatePicker>
                         </div>
                     </div>
                     <div className='text-center'>
-                        <button type='button' className='btn btn-warning w-full my-6 font-bold rounded-3xl'>Create Assignment</button>
+                        <button type='submit' className='btn btn-warning w-full my-6 font-bold rounded-3xl'>Create Assignment</button>
                     </div>
 
                 </form>
